e130cb2943e54e7025af7a3a17b7a51916a19280
restrada <rafae@DESKTOP-0TT2993>
2026-07-22 07:23:07 -0500

    prueba 2

--- HttpTriggerFunction.cs
+++ HttpTriggerFunction.cs
@@ -32,7 +32,7 @@ public class HttpTriggerFunction
 
         var responseMessage = string.IsNullOrEmpty(name)
             ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            : $"Hello1, {name}. This HTTP triggered function executed successfully.";
+            : $"Hello2, {name}. This HTTP triggered function executed successfully.";
 
         await response.WriteStringAsync(responseMessage);
 
