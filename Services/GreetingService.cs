3ddd0481dde34b7b61f707560bbd907d92e7061d
restrada <rafae@DESKTOP-0TT2993>
2026-07-22 11:07:34 -0500

    test 11

--- Services/GreetingService.cs
+++ Services/GreetingService.cs
@@ -5,7 +5,7 @@ public static class GreetingService
     public static string BuildGreeting(string? name)
     {
         return string.IsNullOrWhiteSpace(name)
-            ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.1"
+            ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.2"
             : $"Hello5, {name}. This HTTP triggered function executed successfully.";
     }
 }
